a77b22173cccd905793ce24e779b452649540259
Indira Dayo <indiradayo@live.com>
2018-11-19 14:30:13 +0700

    ganti login

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -50,12 +50,8 @@ module.exports = {
                         first_name: user.first_name,
                         last_name: user.last_name,
                         email: user.email,
-                        shop_list: user.shop_list,
-                        phone: user.phone,
-                        address: user.address,
                         image: user.image,
-                        money: user.money,
-                        transactions: user.transactions
+                        article_list: user.article_list
                     })
                     res.status(200).json({
                         message: 'Login successfully',
